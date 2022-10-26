1cdab2d37cc7f8a9d27b59371f45e08bbb54c522
luksvrd <ljvy9b@gmail.com>
2022-10-25 20:45:00 -0500

    create elements for main tag

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 //  TODO: grab the form and the main tag
 const form = document.querySelector("form");
 const main = document.querySelector("main");
+
+// TODO: create elements that will end up inside the main tag
+const h1 = document.createElement("h1");
+const h2 = document.createElement("h2");
+const small = document.createElement("small");
+const p = document.createElement("p");
