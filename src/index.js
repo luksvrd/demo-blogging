07363c2c1385b52d8a598c72312b0e90c6b8ebb0
luksvrd <ljvy9b@gmail.com>
2022-10-25 21:12:06 -0500

    publish blog post from the form

--- src/index.js
+++ src/index.js
@@ -7,3 +7,25 @@ const h1 = document.createElement("h1");
 const h2 = document.createElement("h2");
 const small = document.createElement("small");
 const p = document.createElement("p");
+
+//  TODO: add event listener for the form submition event
+// whenever dealing w a submittion event you gotta have the .preventDefault in there
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  //  TODO: use formdata to log out hte values that the user put in with the namese of inputs
+  // event.target is whatever element caused the event. this case what was submitted? the form itself
+
+  const formData = new FormData(event.target);
+  const values = Object.fromEntries(formData.entries());
+
+  // Object Destructuring
+  const { title, subtitile, author, content } = values;
+
+  h1.textContent = title;
+  h2.textContent = subtitle;
+  small.textContent = author;
+  p.textContent = content;
+
+  main.append(h1, h2, small, p);
+});
